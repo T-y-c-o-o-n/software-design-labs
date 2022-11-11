5ba05b3c3d01192cc96d833bc0f59ff0bdf7bc00
Tycoon <ntpmosakk070601@gmail.com>
2022-11-11 21:03:56 +0300

    added HashTagCounter and VkClient

---
+++ lab-2/src/main/java/hashtagcounter/HashTagCounter.java
@@ -0,0 +1,30 @@
+package hashtagcounter;
+
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTagCounter {
+    private final VkClient vkClient = new VkClient();
+
+    public List<Integer> hashTagCountPerLastHours(String hashTag, int lastHours) throws ClientException, ApiException {
+        if (lastHours < 1 || lastHours > 24) {
+            throw new IllegalArgumentException("lastHours must be >= 1 and <= 24");
+        }
+        List<Integer> counts = new ArrayList<>();
+        for (int hoursAgo = 1; hoursAgo <= lastHours; ++hoursAgo) {
+            int count = vkClient.count(hashTag, hoursAgo);
+            counts.add(count);
+        }
+        return counts;
+    }
+
+    public static void main(String[] args) throws ClientException, ApiException {
+        List<Integer> counts = new HashTagCounter().hashTagCountPerLastHours("", 3);
+        for (int count : counts) {
+            System.out.println(count);
+        }
+    }
+}
---
+++ lab-2/src/main/java/hashtagcounter/VkClient.java
@@ -0,0 +1,39 @@
+package hashtagcounter;
+
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.ServiceActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.newsfeed.responses.SearchResponse;
+import com.vk.api.sdk.queries.newsfeed.NewsfeedSearchQuery;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class VkClient {
+    private VkApiClient vk;
+
+    public VkClient() {
+        TransportClient transportClient = new HttpTransportClient();
+        vk = new VkApiClient(transportClient);
+    }
+
+    public int count(String hashTag, int hourAgo) throws ClientException, ApiException {
+        long startTime = Instant.now().minus(hourAgo, ChronoUnit.HOURS).getEpochSecond();
+        long endTime = Instant.now().minus(hourAgo - 1, ChronoUnit.HOURS).getEpochSecond();
+        System.out.println(startTime + " --- " + endTime);
+        NewsfeedSearchQuery search = vk.newsfeed()
+            .search(
+                new ServiceActor(
+                    51473962,
+                    "vk1.a.M0Q6-B0l1kRsENNcij1ZvugFOi_Qrhx2nB51LiS_4MfdFy2NZyaAO1TXDrNPDrn2XiIrcT0JMfyV6S3kMQiu6ic5jaVwGJJID-Tb7X8mTSqtUL8tLYl8hOf8F4a29zOpgcJP-mRtUL_p4Ox79ptFZbPsjBJLbGCwTWxgicz1I73mYu1tsqCmC4hCgBQ67203"
+                )
+            ).q("#" + hashTag)
+            .startTime((int) startTime)
+            .endTime((int) endTime);
+        SearchResponse searchResponse = search.execute();
+        return searchResponse.getCount();
+    }
+}
